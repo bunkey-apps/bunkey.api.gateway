57ae38be1f0ccc0a333f03928477b43382534101
Ernesto Rojas <ernesto20145@gmail.com>
2018-09-23 23:13:10 -0300

    Added polices to url-signature router.

--- api/routers/index.js
+++ api/routers/index.js
@@ -2,9 +2,10 @@ import Router from 'koa-router';
 
 const router = new Router({ prefix: '/v1' });
 const { AuthController } = cano.app.controllers;
+const { bearer, jwt } = AuthPolicies;
 
 router.post('/recovery-password', AuthController.recoveryPassword);
 router.put('/recovery-password', AuthController.updatePassword);
-router.post('/url-signature', ObjectController.getUrlSignature);
+router.post('/url-signature', bearer, jwt, ObjectController.getUrlSignature);
 
 module.exports = router;
